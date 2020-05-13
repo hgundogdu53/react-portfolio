7af63c97e24b966d43eeb1199fb85ece5872af4f
Huseyin Gundogdu <hgundogdu53@gmail.com>
2020-05-13 03:21:23 -0700

    updated

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -8,10 +8,10 @@ function AboutMe() {
         Bay Area. I am an enthusiast to develop websites, especially using
         HTML/CSS, Bootstrap, JavaScript, JQuery, React, NodeJS, Express, NextJS,
         MongoDB, GraphQL and PostgreSQL. Besides that, Cloud computing is one of
-        the my interests and I achieved AWS Solution Architect-Associate
-        certificate. Outside of coding, my interest areas are swimming, soccer,
-        hiking and black tea. Feel free to get in touch by any means of
-        communication.
+        the my interests and I achieved AWS Solution Architect-Associate and
+        freeCodeCamp Frontend Libraries certificates. Outside of coding, my
+        interest areas are swimming, soccer, hiking and black tea. Feel free to
+        get in touch by any means of communication.
       </p>
     </div>
   );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,42 +1,62 @@
-import React from 'react';
+import React from "react";
 
 function LandingPage() {
-    return (
-        <div className="landing-page" style={{ width: '100%', margin: 'auto' }}>
-            <img
-                src="https://github.com/hgundogdu53/myPortfolio-hg/blob/master/my%20icon.png?raw=true"
-                alt="my-icon"
-                className="my-icon"
-            />
+  return (
+    <div className="landing-page" style={{ width: "100%", margin: "auto" }}>
+      <img
+        src="https://github.com/hgundogdu53/myPortfolio-hg/blob/master/my%20icon.png?raw=true"
+        alt="my-icon"
+        className="my-icon"
+      />
 
-            <div className="banner-text">
-                <h1 className="animated 1 fadeInLeft delay-2s">Huseyin Gundogdu</h1>
-                <h2 className="animated 1 fadeInRight delay-2s">UI Developer</h2>
-                <hr />
-                <p>HTML/CSS | Bootstrap | JavaScript | React | JQuery | NodeJS | Express | PostgreSQL | AWS</p>
-            </div>
+      <div className="banner-text">
+        <h1 className="animated 1 fadeInLeft delay-1.2s">Huseyin Gundogdu</h1>
+        <h2 className="animated 1 fadeInRight delay-1.8s">
+          Full Stack Web Developer
+        </h2>
+        <hr />
+        <p>
+          HTML/CSS | Bootstrap | JavaScript | React | JQuery | NodeJS | Express
+          | MongoDB | GraphQL | PostgreSQL | AWS
+        </p>
+      </div>
 
-            <div className="social-links">
-                {/* LinkedIn */}
-                <a href="https://www.linkedin.com/in/huseyin-developer/" rel="noopener noreferrer" target="_blank">
-                    <i className="fa fa-linkedin-square" aria-hidden="true" />
-                </a>
-                {/* Github */}
-                <a href="https://github.com/hgundogdu53" rel="noopener noreferrer" target="_blank">
-                    <i className="fa fa-github-square" aria-hidden="true" />
-                </a>
-                {/* Twitter */}
-                <a href="https://twitter.com/hgundogdu53" rel="noopener noreferrer" target="_blank">
-                    <i className="fa fa-twitter-square" aria-hidden="true" />
-                </a>
-                {/* MovieX */}
-                <a href="https://moviex.now.sh" rel="noopener noreferrer" target="_blank">
-                    <i className="fa fa-film" aria-hidden="true" />
-                </a>
-            </div>
-
-        </div>
-    )
+      <div className="social-links">
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/huseyin-developer/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i className="fa fa-linkedin-square" aria-hidden="true" />
+        </a>
+        {/* Github */}
+        <a
+          href="https://github.com/hgundogdu53"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i className="fa fa-github-square" aria-hidden="true" />
+        </a>
+        {/* Twitter */}
+        <a
+          href="https://twitter.com/hgundogdu53"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i className="fa fa-twitter-square" aria-hidden="true" />
+        </a>
+        {/* MovieX */}
+        <a
+          href="https://moviex.now.sh"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i className="fa fa-film" aria-hidden="true" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
