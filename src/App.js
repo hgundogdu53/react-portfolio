f6c461066cba1cd24f51cc0aae1c1d3b35097ea3
Huseyin Gundogdu <hgundogdu53@gmail.com>
2020-07-07 12:39:36 -0700

    ui-gradient added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import AboutMe from './components/AboutMe';
-import ContactPage from './components/ContactPage';
-import Projects from './components/Projects';
-import LandingPage from './components/LandingPage';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import AboutMe from "./components/AboutMe";
+import ContactPage from "./components/ContactPage";
+import Projects from "./components/Projects";
+import LandingPage from "./components/LandingPage";
+import "./App.css";
 import { slide as Menu } from "react-burger-menu";
-
 
 const App = () => {
   return (
@@ -14,26 +13,26 @@ const App = () => {
       <Menu right>
         <a className="menu-item" href="/">
           Home
-                </a>
+        </a>
         <a className="menu-item" href="/aboutme">
           AboutMe
-                 </a>
+        </a>
         <a className="menu-item" href="/projects">
           Projects
-                 </a>
+        </a>
         <a className="menu-item" href="/Contact">
           Contact
-                 </a>
+        </a>
       </Menu>
+
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/aboutme" component={AboutMe} />
         <Route path="/contact" component={ContactPage} />
         <Route path="/projects" component={Projects} />
       </Switch>
-    </div >
+    </div>
   );
-}
-
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, document.getElementById('root'));
-
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
